9a4308b890c5342fd7558ff09d0e0e97680092e3
Olanrewaju <olanrewaju@seamlesshr.com>
2023-04-11 06:21:47 +0100

    str replace routes to remove extension

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+$uri = str_replace('.php', '', $url);
 
 $routes = [
-    '/index.php' => 'controllers/index.php',
-    '/about.php' => 'controllers/about.php',
-    '/contact.php' => 'controllers/contact.php',
-    '/mission.php' => 'controllers/mission.php'
+    '/index' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/mission' => 'controllers/mission.php'
 ];
-
 
 if (array_key_exists($uri, $routes)){
     require $routes[$uri];
