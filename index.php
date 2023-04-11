168d3f6395abd70c81cd3dc02b440b94b1bafd7b
Olanrewaju <olanrewaju@seamlesshr.com>
2023-04-11 10:02:36 +0100

    add base url

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'functions.php';
 
 $url = parse_url($_SERVER['REQUEST_URI'])['path'];
 $uri = str_replace('.php', '', $url);
-
+$base_url = '/';
 $routes = [
     '/' => 'controllers/index.php',
     '/index' => 'controllers/index.php',
@@ -15,4 +15,7 @@ $routes = [
 
 if (array_key_exists($uri, $routes)){
     require $routes[$uri];
+}else{
+    header("Location: $base_url");
+    exit;
 }
